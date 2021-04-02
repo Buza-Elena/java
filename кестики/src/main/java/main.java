18e0368f05b4ddd6150f09e005e7241f17e0d362
Buza-Elena <lv-9393@mail.ru>
2021-04-02 17:44:47 +0600

    1

---
+++ кестики/src/main/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public class Main {
+        public static void main (String[] args){
+            System.out.println("Запуск игры");
+            System.out.println("конец...");
+
+        }
+    }
+}
